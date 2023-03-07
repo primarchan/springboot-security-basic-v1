0352ab4cd2ef28991fdf7e23bdfb669c25b7e766
primarchan <primarchan@sanhait.co.kr>
2023-03-07 18:08:55 +0900

    [Springboot Security] ch2 - Security 설정

--- src/main/java/com/example/security1/controller/IndexController.java
+++ src/main/java/com/example/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -13,6 +14,42 @@ public class IndexController {
         // (Spring 에서 권장하는 template engine) Mustache - 기본 폴더 : src/main/resources
         // View Resolver 설정 : templates (prefix), .mustache (suffix) - Mustache 사용 시 생략 가능
         return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @ResponseBody
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @ResponseBody
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @ResponseBody
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @ResponseBody
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @ResponseBody
+    @GetMapping("/joinProc")
+    public String joinProc() {
+        return "회원가입 완료됨!";
     }
 
 }
